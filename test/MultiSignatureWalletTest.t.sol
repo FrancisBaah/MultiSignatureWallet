55f219794aaacbf10dbe608f117b805b6f9b9b2b
FrancisBaah <baahfrancis88@gmail.com>
2023-11-12 01:21:28 +0400

    deployed smart contract

--- test/MultiSignatureWalletTest.t.sol
+++ test/MultiSignatureWalletTest.t.sol
@@ -1,14 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {MultiSignatureWallet} from "../src/MultiSignatureWallet.sol";
 
 contract CounterTest is Test {
     MultiSignatureWallet public multiSignatureWallet;
+    address[] authorizedSigners;
+    uint256 quorum = 2; 
+
 
     function setUp() public {
-        // multiSignatureWallet = new MultiSignatureWallet();
+        multiSignatureWallet = new MultiSignatureWallet(authorizedSigners, quorum);
+    }
+
+    function testIfOwnerIsTheOneWhoDeploy() public{
+        vm.prank(address(0));
+        // console.log(multiSignatureWallet.balance);
     }
 
 }
